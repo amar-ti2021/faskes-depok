df4a392763ae58a6d6781d469d0fc155e39e351a
amar-ti2021 <muha21095ti@student.nurulfikri.ac.id>
2022-06-28 22:33:31 +0700

    Create register view

---
+++ application/views/register.php
@@ -0,0 +1,97 @@
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Faskes Depok | Registration Page</title>
+
+    <!-- Google Font: Source Sans Pro -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&amp;display=fallback">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="<?= base_url('assets/plugins/fontawesome-free/css/all.min.css'); ?>">
+    <!-- icheck bootstrap -->
+    <link rel="stylesheet" href="<?= base_url('assets/plugins/icheck-bootstrap/icheck-bootstrap.min.css'); ?>">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="<?= base_url('assets/dist/css/adminlte.min.css'); ?>">
+</head>
+
+<body class="register-page" style="min-height: 570.781px;">
+    <div class="register-box">
+        <div class="register-logo">
+            <a href="home"><b>Admin</b>LTE</a>
+        </div>
+
+        <div class="card">
+            <div class="card-body register-card-body">
+                <p class="login-box-msg">Registrasi Akun Baru</p>
+                <?php echo form_open('register') ?>
+                <div class="mb-3">
+                    <div class="input-group">
+                        <input name="username" type="text" class="form-control" placeholder="Username">
+                        <div class="input-group-append">
+                            <div class="input-group-text">
+                                <span class="fas fa-user"></span>
+                            </div>
+                        </div>
+                    </div>
+                    <?= form_error('username', '<small class="text-danger ml-2">', '</small>'); ?>
+                </div>
+                <div class="mb-3">
+                    <div class="input-group">
+                        <input name="email" type=" email" class="form-control" placeholder="Email">
+                        <div class="input-group-append">
+                            <div class="input-group-text">
+                                <span class="fas fa-envelope"></span>
+                            </div>
+                        </div>
+                    </div>
+                    <?= form_error('email', '<small class="text-danger ml-2">', '</small>'); ?>
+                </div>
+                <div class="mb-3">
+                    <div class="input-group">
+                        <input name="password_1" type="password" class="form-control" placeholder="Password">
+                        <div class="input-group-append">
+                            <div class="input-group-text">
+                                <span class="fas fa-lock"></span>
+                            </div>
+                        </div>
+                    </div>
+                    <?= form_error('password_1', '<small class="text-danger ml-2">', '</small>'); ?>
+                </div>
+                <div class="mb-3">
+                    <div class="input-group">
+                        <input name="password_2" type="password" class="form-control" placeholder="Retype password">
+                        <div class="input-group-append">
+                            <div class="input-group-text">
+                                <span class="fas fa-lock"></span>
+                            </div>
+                        </div>
+                    </div>
+                    <?= form_error('password_2', '<small class="text-danger ml-2">', '</small>'); ?>
+                </div>
+                <div class="row">
+                    <!-- /.col -->
+                    <div class="col-12">
+                        <button type="submit" class="btn btn-primary btn-block">Register</button>
+                    </div>
+                    <!-- /.col -->
+                </div>
+                <?php echo form_close(); ?>
+                <a href="login" class="text-center">Sudah Punya Akun? Login!</a>
+            </div>
+            <!-- /.form-box -->
+        </div><!-- /.card -->
+    </div>
+    <!-- /.register-box -->
+
+    <!-- jQuery -->
+    <script src="<?= base_url('assets/plugins/jquery/jquery.min.js'); ?>"></script>
+    <!-- Bootstrap 4 -->
+    <script src="<?= base_url('assets/plugins/bootstrap/js/bootstrap.bundle.min.js'); ?>"></script>
+    <!-- AdminLTE App -->
+    <script src="<?= base_url('assets/dist/js/adminlte.min.js'); ?>"></script>
+
+
+</body>
+
+</html>
