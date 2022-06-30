12ce0c9e4517542fbfe52e3f7a8ea299fd1d6908
amar-ti2021 <muha21095ti@student.nurulfikri.ac.id>
2022-06-30 12:20:08 +0700

    Create search method

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -1,9 +1,21 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
-class Home extends CI_Controller{
-    public function index(){
-
-        $this->load->view('home/index');
+class Home extends CI_Controller
+{
+    public function index()
+    {
+        $this->load->model("Faskes_Model", "faskes");
+        $data = array('faskes' => $this->faskes->getAll());
+        $this->load->view('home/index', $data);
+    }
+    public function search($keyword)
+    {
+        $this->load->model("Faskes_Model", "faskes");
+        $data = array('faskes' => $this->faskes->search($keyword));
+        if (!$data['faskes']) {
+            echo '<h3 class="text-center"> Fasilitas Tidak Ditemukan </h3>';
+        }
+        $this->load->view('home/search', $data);
     }
 }
